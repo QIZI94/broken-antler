5d4529f56cd45d3246d0cf54738816018e6e1839
QIZI94 <qizi94@gmail.com>
2026-01-02 20:16:08 +0100

    Add shared step check
    Some cleanup and doxygen comments.

--- SchedPWM.cpp
+++ SchedPWM.cpp
@@ -64,13 +64,17 @@ void ScheduledPWM::setLedPWM(LedID ledId, BrightnessType brightness){
 			!isLedAssigned(ledId, currentStep.bitStorage) && 
 			nextNode != steps.end()
 		){
+			if(!isLedStepShared(ledId, previousStep.bitStorage, currentStep.bitStorage)){
+
+			
 #ifdef DEBUG_SCHED_PMW
-			Serial.println("removing previous");
+				Serial.println("removing previous");
 #endif
-			previousStep.nextIsrTime = currentStep.nextIsrTime;
-			currentNode = steps.removeAfter(previousNode);
-			pastInstanceRemoved = true;
-			continue;
+				previousStep.nextIsrTime = currentStep.nextIsrTime;
+				currentNode = steps.removeAfter(previousNode);
+				pastInstanceRemoved = true;
+				continue;
+			}
 		}
 		
 
@@ -111,146 +115,6 @@ void ScheduledPWM::setLedPWM(LedID ledId, BrightnessType brightness){
 			newStepNode->value.bitStorage		
 		);
 	}
-	/*
-
-
-	StepNode* currentNode = steps.begin();
-
-	if(brightness == 0){
-		unassignLed(ledId, currentNode->value.bitStorage);
-	}
-	else {
-		assignLed(ledId, currentNode->value.bitStorage);
-	}
-	
-	//StepNode* currentNode = steps.begin();
-	currentNode = steps.nextNode(currentNode);;
-	StepNode* previousNode = steps.begin();
-	StepNode* nodeToInsertAfter = nullptr;
-	while(1){
-		if(currentNode == steps.end()){
-			nodeToInsertAfter = previousNode;
-			break;
-		}
-
-		PWMStep& currentStep = currentNode->value;
-		if(nodeToInsertAfter == nullptr && currentStep.nextIsrTime > brightness){
-			nodeToInsertAfter = previousNode;
-			unassignLed(ledId, currentStep.bitStorage);
-		}
-		else if(currentStep.nextIsrTime < brightness) {
-			assignLed(ledId, currentStep.bitStorage);
-		}
-		else {
-			unassignLed(ledId, currentStep.bitStorage);
-			previousNode = currentNode;
-			currentNode = steps.nextNode(currentNode);
-			continue;
-		}
-
-		if(isLedExclusive(ledId, currentStep.bitStorage)){
-			unassignLed(ledId, currentStep.bitStorage);
-			currentNode = steps.removeAfter(previousNode);
-		}
-		else if(!isLedAssigned(ledId, currentStep.bitStorage)){
-			break;
-		}
-		else {
-			previousNode = currentNode;
-			currentNode = steps.nextNode(currentNode);
-		}		
-	}
-	if(nodeToInsertAfter != nullptr){
-		StepNode* referenceNode = nodeToInsertAfter != steps.end() ? nodeToInsertAfter : steps.begin();
-		PWMStep& referenceStep = referenceNode->value;
-		
-		StepNode* newStepNode = steps.insertAfter(nodeToInsertAfter, referenceStep);
-		Serial.println(steps.indexByNode(newStepNode));
-
-		referenceStep.nextIsrTime = brightness;
-		unassignLed(
-			ledId,
-			newStepNode->value.bitStorage		
-		);
-	}
-	*/
-
-	
-
-
-/*
-	PWMStep fakeStep{};
-	PWMStep* previousStep = &fakeStep;
-	//PWMStep* assigmentStep
-	PWMStep* finalStep = nullptr;
-
-	PWMStep* lastDimmerStep = &fakeStep;
-
-	
-	//assignLed(ledId, currentStep->bitStorage);
-	
-	
-	uint8_t previousIdx;
-
-	bool foundIdenticalStep = false;
-
-
-	PWMStep& firstStep = steps[0];
-	if(brightness == 0){
-		unassignLed(ledId, firstStep.bitStorage);
-	}
-	else {
-		assignLed(ledId, firstStep.bitStorage);
-	}
-
-	//lastDimmerStep = &firstStep;
-	uint8_t stepIdx = firstStep.nextIndex;
-	
-	while(stepIdx != (LED_COUNT + 1)){
-		
-		PWMStep& currentStep = steps[stepIdx];
-
-
-		if(isLedExclusive(ledId, currentStep.bitStorage) && stepIdx != 0){
-			stepIdx = deallocateStep(stepIdx, previousIdx);
-		}
-		else {
-			previousIdx = stepIdx;
-			stepIdx = currentStep.nextIndex;
-		}
-		if(previousStep->nextIsrTime < brightness){
-			assignLed(ledId, currentStep.bitStorage);
-			lastDimmerStep = &currentStep;
-			//previousStep = currentStep;
-		}
-		else if(previousStep->nextIsrTime > brightness){
-			unassignLed(ledId, currentStep.bitStorage);
-		}
-		else{
-			unassignLed(ledId, currentStep.bitStorage);
-			foundIdenticalStep = false;
-		}
-		
-		previousStep = &currentStep;
-	}
-
-	if(foundIdenticalStep){
-		return;
-	}
-
-
-	uint8_t newStepIdx = allocateStep();
-
-	
-	PWMStep& newStep = steps[newStepIdx];
-	newStep.nextIsrTime = lastDimmerStep->nextIsrTime;
-	lastDimmerStep->nextIsrTime = brightness;
-	newStep.nextIndex = lastDimmerStep->nextIndex;
-	lastDimmerStep->nextIndex = newStepIdx;
-	newStep.bitStorage |= lastDimmerStep->bitStorage;
-	unassignLed(ledId, newStep.bitStorage);
-
-*/
 }
 
 bool ScheduledPWM::pwmISR(){
--- SchedPWM.h
+++ SchedPWM.h
@@ -25,38 +25,93 @@ private:
 		BitStorageType bitStorage{};
 	};
 
-
-
 	using StepList = FixedForwardList<LED_COUNT + 1, PWMStep, uint8_t, panicOnStepError>;
 	using StepNode = StepList::Node;
 
 public:
-	ScheduledPWM(BrightnessType minBrightness = 0, BrightnessType maxBrightness = DEFAULT_MAX_BRIGHTNESS) : minBrightness(minBrightness), maxBrightness(maxBrightness){}
+	ScheduledPWM(
+		BrightnessType minBrightness = DEFAULT_MIN_BRIGHTNESS,
+		BrightnessType maxBrightness = DEFAULT_MAX_BRIGHTNESS
+	) : minBrightness(minBrightness), maxBrightness(maxBrightness){}
 	
+	/**
+	 * Set PWM Duty cycle for specific Led by brightness/time.
+	 * 
+	 * @param ledId used for unique identification of led
+	 * @param brightness brightness based on which duty cycle time is setup
+	*/
 	void setLedPWM(LedID ledId, BrightnessType brightness);
-
+	
+	/**
+	 * This will process single step each time its called in user provided timmed interrupt.
+	 * 
+	 * @returns TRUE when last PWM Step has been processed, otherwise FALSE.
+	*/
 	bool pwmISR();
 protected:
-
+	/**
+	 * Assign Led to scheduled step storage in PWM Cycle.
+	 * This has to be assigned in a way for isLedAssigned() and isLedExclusive to detect assigned Led
+	 * (called from setLedPWM())
+	 * 
+	 * @param ledId used for unique identification of led
+	 * @param stepStorage writable step storage to assign Led into
+	*/
 	virtual void assignLed(LedID ledId, BitStorageType& stepStorage){
 		stepStorage |= 0x01 << ledId;
 	}
-
+	/**
+	 * Unassigned Led to scheduled step storage in PWM Cycle.
+	 * This has to be unassigned in a way for isLedAssigned() and isLedExclusive to not detect unassigned Led
+	 * (called from setLedPWM())
+	 * 
+	 * @param ledId used for unique identification of led
+	 * @param stepStorage writable step storage to unassign Led from
+	*/
 	virtual void unassignLed(LedID ledId, BitStorageType& stepStorage){
 		stepStorage &= ~(0x01 << ledId);
 	}
-
+	/**
+	 * Read and apply stepStorage to turn ON or OFF all described Leds.
+	 * (called from pwmISR())
+	 *  
+	 * @param stepStorage read only step storage 
+	*/
 	virtual void processLedStep(const BitStorageType& stepStorage){
 
 	
 	}
 
+	/**
+	 * Use next time for scheduling next timer interrupt
+	 * (called from pwmISR())
+	 *  
+	 * @param nextTime next scheduled timmed interupt to apply next step 
+	*/
 	virtual void setupNextIsrTime(BrightnessType nextTime){
 		
 	}
+
+	/**
+	 * Check if specific Led is assigned.
+	 * (called from setLedPWM())
+	 *  
+	 * @param ledId used for unique identification of Led
+	 * @param stepStorage read only step storage to check unique Led presence
+	 * @returns If Led is assigned in stepStorage return TRUE, otherwise return FALSE.
+	*/
 	virtual bool isLedAssigned(LedID ledId, const BitStorageType& stepStorage){
 		return ((0x01 << ledId) & stepStorage) != 0;
 	}
+
+	/**
+	 * Check if Led is only one assigned in stepStorage.
+	 * (called from setLedPWM())
+	 *  
+	 * @param ledId used for unique identification of Led
+	 * @param stepStorage read only step storage to check unique Led presence
+	 * @returns If Led is uniqually assigned in stepStorage return TRUE, otherwise return FALSE.
+	*/
 	virtual bool isLedExclusive(LedID ledId, const BitStorageType& stepStorage){
 		if(stepStorage == 0){
 			return false;
@@ -64,7 +119,23 @@ protected:
 		BitStorageType inverseLedMask = ~(0x01 << ledId);
 		return (inverseLedMask & stepStorage) == 0;
 	}
+
+	virtual bool isLedStepShared(LedID ledId, const BitStorageType& previousStepStorage, const BitStorageType& currentStepStorage){
+		uint8_t xoredStep = previousStepStorage ^ currentStepStorage;
+		Serial.println(xoredStep, BIN);
+		return !isLedExclusive(ledId, xoredStep);
+	}
+
+	/**
+	 * Called on PWM Duty Cycle start.
+	 * (called from pwmISR())
+	*/
 	virtual void onDutyCycleBegin(){}
+
+	/**
+	 * Called on last PWM Duty Cycle after after last step is processed.
+	 * (called from pwmISR())
+	*/
 	virtual void onDutyCycleEnd(){}
 
 	
