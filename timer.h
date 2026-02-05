6cd0fed1330f8d07e486c9def86144eb4f4ae2a1
QIZI94 <qizi94@gmail.com>
2026-02-05 17:10:33 +0100

    Change timer counter numeric type to uin16_t

--- timer.h
+++ timer.h
@@ -3,7 +3,7 @@
 #include "timedexecution.h"
 
 
-using StaticTimer1ms = StaticTimer<1>;
+using StaticTimer1ms = StaticTimer<1, uint16_t>;
 using TimedExecution1ms = TimedExecution<StaticTimer1ms>;
 
 extern void initTimers();
